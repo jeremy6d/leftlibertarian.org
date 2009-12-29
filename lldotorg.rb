abd6db80f128f5caa516d83860208bafda5c0ab6
Jeremy Weiland <jeremy6d@gmail.com>
2009-12-29 12:03:03 -0500

    analytics tweak

--- lldotorg.rb
+++ lldotorg.rb
@@ -231,14 +231,6 @@ class LLDotOrg
 	end
 	
 	def tracking_code
-	  %q{<script type="text/javascript">
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-    </script>
-    <script type="text/javascript">
-    try {
-    var pageTracker = _gat._getTracker("UA-3295516-1");
-    pageTracker._trackPageview();
-    } catch(err) {}</script>}
+	  File.read("templates/analytics_code.html")
 	end
 end
