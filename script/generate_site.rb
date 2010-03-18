ed191edff5c63ee9e3b36952414f0f67527d2d35
Jeremy Weiland <jeremy6d@gmail.com>
2010-03-17 23:07:42 -0400

    script/generate_site.rb will email me on an exception

--- script/generate_site.rb
+++ script/generate_site.rb
@@ -1,3 +1,20 @@
 require 'rubygems'
 require 'lldotorg'
-LLDotOrg.new('production').generate_site!
+require 'net/smtp'
+begin
+  LLDotOrg.new('production').generate_site!
+rescue Exception => e
+  from = "LeftLibertarian.org generate script <leftlibertarian.org@potentiator>"
+  to = "jeremy6d@gmail.com"
+  msg = <<-END_OF_MESSAGE
+  From: #{from}
+  To: #{to}
+  Subject: SCRIPT FAILED
+	
+  #{e.inspect}
+  END_OF_MESSAGE
+	
+	Net::SMTP.start('localhost') do |smtp|
+		smtp.send_message msg, from, to
+	end
+end
